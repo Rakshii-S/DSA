e7e51b25d976a722710e3911c9cc4e3c6f518c48
Rakshitha <rakshithasullal@gmail.com>
2024-11-06 11:21:44 +0530

    Bubble Sort Algorithm

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int BubbleSort(int arr[], int n)
+{
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+int main()
+{
+    int n, arr[30];
+    cout<<"Enter the range: ";
+    cin>>n;
+    cout<<"Enter "<<n<<" elements to the array: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Array after performing bubble sort: ";
+    BubbleSort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
