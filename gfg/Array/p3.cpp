31f34c737394c2660b41ba38a9c13dafb6025170
Rakshitha <rakshithasullal@gmail.com>
2025-01-05 10:00:53 +0530

    Maximum product of triplet (sunsequence of 3) in array

--- gfg/Array/p3.cpp
+++ gfg/Array/p3.cpp
@@ -1,10 +1,42 @@
 //Three Great Candidates
 //Maximum product of a triplet(subsequence of size 3) in array
 #include <iostream>
+#include <climits>
 #include <vector>
-
 using namespace std;
 
+int ThreeGreatCandidates(vector<int> &num)
+{
+    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+    int product1 , product2;
+    for(int i=0;i<num.size();i++)
+    {
+        if(num[i]>max1)
+        {
+            max3 = max2;
+            max2 = max1;
+            max1 = num[i];
+        }else if(num[i] > max2 && num[i] < max1)
+        {
+            max3 = max2;
+            max2 = num[i];
+        }else if(num[i]>max3 && num[i]<max2 && num[i]<max1){
+            max3 = num[i];
+        }
+
+        if(num[i]<min1)
+        {
+            min2 = min1;
+            min1 = num[i];
+        }else if(num[i]<min2 && num[i]>min1){
+            min2 = num[i];
+        }
+    }
+    product1 = max1*max2*max3;
+    product2 = max1*min1*min2;
+    return max(product1, product2);
+}
 int main()
 {
     int n;
@@ -16,10 +48,19 @@ int main()
         cout<<"Array size should be greater than or equal to 3.";
         return 0;
     }
+
     vector<int> num(n,0);
     cout<<"Enter the values: ";
     for(int i=0;i<n;i++)
     {
         cin>>num[i];
     }
+
+    int ans  = ThreeGreatCandidates(num);
+    if(ans == -1)
+        cout<<"Maximum product of triplets not found.";
+    else
+        cout<<"Maximum product of triplets is: "<<ans;
+
+    return 0;
 }
