485d1f7f889298bc922a36036defdad48272c08e
Rakshitha <rakshithasullal@gmail.com>
2025-01-05 10:52:08 +0530

    Maximum consecutive one's in a binary array

--- gfg/Array/p4.cpp
+++ gfg/Array/p4.cpp
@@ -1,5 +1,19 @@
-// Maximu consecutive one's( or Zero's) in a binary array
+// Maximu consecutive one's in a binary array
 //Three Great Candidates
+
+/*Given a binary array, find the count of a maximum number of consecutive 1s present in the array.
+
+Examples : 
+
+Input: arr[] = {1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1}
+Output: 4
+Explanation: The maximum number of consecutive 1’s in the array is 4 from index 8-11.
+
+
+Input: arr[] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
+Output: 1
+Explanation: The maximum number of consecutive 0’s in the array is 1 from index 0-1.*/
+
 #include <iostream>
 #include <climits>
 #include <vector>
@@ -8,22 +22,17 @@ using namespace std;
 int ConsecutiveValues(vector<int> &num)
 {
     int ans = 0, count = 0;
-    int currEle = num[0];
     for (int i = 1; i < num.size(); i++)
     {
-        if (num[i] == currEle)
+        if(num[i] == 1)
         {
-            count++;  // Increment count if the same element continues
-        }
-        else
-        {
-            ans = max(ans, count);  // Update maximum before resetting count
-            currEle = num[i];       // Update current element
-            count = 1;              // Reset count for the new sequence
+            count++;
+        }else{
+            ans = max(ans, count);
+            count = 0;
         }
     }
-
-    ans = max(ans, count);  // Final update to ensure last sequence is considered
+    ans = max(ans, count);
     return ans;
 }
 int main()
