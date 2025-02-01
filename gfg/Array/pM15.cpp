27dc873d90353a7cb4b153f9a85737eae34e1adf
Rakshitha <rakshithasullal@gmail.com>
2025-02-01 17:22:12 +0530

    Minimize the Heights II

--- gfg/Array/pM15.cpp
+++ gfg/Array/pM15.cpp
@@ -1,7 +1,6 @@
 //Minimize the Heights II
 
 /*Given an array arr[] denoting heights of N towers and a positive integer K.
-
 For each tower, you must perform exactly one of the following operations exactly once.
 
 Increase the height of the tower by K
